3011e998beb34c0f54c0435000335b352b347357
kangdora <ioprt123@gmail.com>
2024-10-29 12:33:33 +0900

    feat: floating content popup 구현

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const open_popup = document.getElementById('buy-popup-open');
+const popup = document.getElementById('floating-content');
+const background = document.getElementById('popup-background');
+const close_popup = document.getElementById('close-floating-content');
+
+open_popup.addEventListener('click', ()=>{
+    popup.style.display = 'flex';
+    background.style.display = 'flex';
+});
+
+close_popup.addEventListener('click', ()=>{
+    popup.style.display = 'none';
+    background.style.display = 'none';
+})
