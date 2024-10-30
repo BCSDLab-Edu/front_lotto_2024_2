3ea84852dd4ec1cc1aceeba5955162e912d29db0
asa9874 <pjkpjk1129@gmail.com>
2024-10-30 21:40:54 +0900

    Feat: 로또 중복제거

--- main.js
+++ main.js
@@ -5,40 +5,57 @@ const $resultbutton=document.getElementsByClassName("result")[0];
 const $modal=document.getElementsByClassName("modal")[0];
 const $modalclosex=document.getElementsByClassName("modal-close-x")[0];
 const $restartbutton=document.getElementsByClassName("restart")[0];
+const $winninglottos = document.querySelectorAll('.number-input-container input');
 $modal.style.display = 'none';
 
+let lottos=[]
+
+
+//무작위 로또생성
 function randomnumber(){
-    const RandomRange=30;
+    const RandomRange=30;   //로또숫자범위
     const lottoNumbers = [];
-    for (let i = 0; i < 6; i++) {
-        lottoNumbers.push(Math.floor(Math.random() * RandomRange));
+    while(true){
+        const num= Math.floor(Math.random() * RandomRange)
+        if(!lottoNumbers.includes(num)){lottoNumbers.push(num);} 
+        if(lottoNumbers.length==6) break;
     }
+    lottos.push(lottoNumbers)
     return lottoNumbers.join(", ");
 }
 
+//로또 구입
 function buyLotto(event){
     event.preventDefault()
+    lottos=[]
     const boughtCount=Math.floor($purchasemoney.value/1000);
     $lottolist.innerHTML = `<span class="lotto-bought-title">총  ${boughtCount}개를 구매하였습니다.</span>`
     for(let count=0;count<boughtCount;count++){
         $lottolist.innerHTML+=
         `<div>🎟️ ${randomnumber()}</div>`
     }
+    console.log(lottos)
 }
 
+//모달 ON/OFF
 function toggleModal(){
     if($modal.style.display === 'flex')$modal.style.display = 'none';
     else $modal.style.display='flex';
 }
 
+
+//로또 결과
 function resultLotto(event){
     event.preventDefault();
     Checkwinning();
     toggleModal();
 }
 
+//당첨로또 체크
 function Checkwinning(){
+    const winning = Array.from($winninglottos).map(win => win.value);
 
+    console.log(winning);
 }
 
 
