f253db9ffc4e0cfbbad44a0a2277e7c911626c2a
kangdora <ioprt123@gmail.com>
2024-10-30 22:00:54 +0900

    feat: 구매 개수 구현

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-const open_popup = document.getElementById('buy-popup-open');
+const open_popup = document.getElementById('checkResult');
 const popup = document.getElementById('floating-content');
 const background = document.getElementById('popup-background');
 const close_popup = document.getElementById('close-floating-content');
+const buy_lottery = document.getElementById('buy-button');
+const money_amount = document.getElementById('money-amount');
 
 async function getLottoNumber(){
     const firstLottoDate = new Date('2002-12-07');
@@ -17,23 +19,26 @@ async function getLottoNumber(){
 
     if (today > lastSaturday) lottoRound += 1;
 
-    let site = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + lottoRound;
+    let site = "https://cors-anywhere.herokuapp.com/https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + (lottoRound-1);
 
+    console.log(lastSaturday);
     try {
         const res = await fetch(site);
-        const data = await res.json();
 
-        let successValue = data['returnValue'];
-        if (successValue === "success") {
+
+        const text = await res.text();
+        const data = JSON.parse(text);
+
+
+        if (data['returnValue'] === "success") {
             let numberList = [];
-            for (let i = 1; i <= 6; i++) {
-                let numberidx = 'drwtNo' + i;
-                numberList.push(data[numberidx]);
+            for (let i = 1; i <= 6; i++){
+                numberList.push(data[`drwtNo${i}`]);
             }
             numberList.push(data['bnusNo']);
             return numberList;
         } else {
-            return false;
+            throw new Error("API 요청에 실패했습니다.");
         }
     } catch (error) {
         console.error('로또 번호를 가져오는 중 오류 발생:', error);
@@ -41,27 +46,76 @@ async function getLottoNumber(){
     }
 }
 
+buy_lottery.addEventListener('click', () => {  // 구매 개수 구현
+    const amountContext = document.getElementById('amount-context');  // 총 ~ 개
+    const lotteryTable = document.getElementById("lottery-table");
+
+    let lottery_amount = money_amount.value / 1000;  // 구매 갯수
+
+    if (lottery_amount > 15) lottery_amount = 15;  // 최대 갯수 제한
+
+    lotteryTable.innerHTML = '';  // table 초기화
+
+    if (lottery_amount > 6){  // 구매한 로또의 개수에 따라서 main-contents 창이 길어짐
+        const mainContents = document.getElementById('main-contents');
+
+        lotteryTable.style.height = `${lottery_amount * 40}px`
+        mainContents.style.height = `${451 + lottery_amount * 40}px`
+    }
+
+
+    function addLotteryList(){  // 로또 랜덤 생성 함수
+        const li = document.createElement("li");
+        li.className = "container";
+
+        const span = document.createElement("span");
+        span.textContent = "🎟️";
+
+        const p = document.createElement("p");
+        let lotteryList = [];
+
+        for (let i = 1; i <= 6; i++){
+            lotteryList.push(Math.floor(Math.random() * 45) + 1);
+        }
+
+        p.textContent = lotteryList.join(", ");
+
+        li.appendChild(span);
+        li.appendChild(p);
+        lotteryTable.appendChild(li);
+    }
+
+    for (let i = 0; i < lottery_amount; i++){
+        addLotteryList();
+    }
+    if (lottery_amount == 0) {
+        amountContext.innerHTML = `<p>아무것도 구매하지 않으셨습니다.</p>`;
+    } else {
+        amountContext.innerHTML = `<p>총 ${lottery_amount}개를 구매하였습니다.</p>`;
+    }
+});
+
 open_popup.addEventListener('click', async ()=>{
     popup.style.display = 'flex';
     background.style.display = 'flex';
-    let lottery = await getLottoNumber();
+    const lottery = await getLottoNumber();
     const lotteryNumber = document.getElementById('lottery-number');
+
     if (lottery){
         let bonus_number = lottery.pop();
         lottery.sort((a, b) => a - b);
-
-        lotteryNumber.innerHTML = `로또 번호: ${lottery.join(', ')}, 보너스 번호: ${bonus_number}`;
+        lotteryNumber.innerHTML = `${lottery.join(', ')} + ${bonus_number}`;
+        lotteryNumber.style.fontSize = '16px';
     } else {
         lotteryNumber.innerHTML = '로또 번호를 가져오는데 실패했습니다.';
     }
+
 })
 
-background.addEventListener('click', ()=>{
-    popup.style.display = 'none';
-    background.style.display = 'none';
-})
+background.addEventListener('click', closePopup);
+close_popup.addEventListener('click', closePopup);
 
-close_popup.addEventListener('click', ()=>{
+function closePopup() {
     popup.style.display = 'none';
     background.style.display = 'none';
-})
+}
